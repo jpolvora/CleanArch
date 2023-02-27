bb0f35fff17c4b1a0eb13fb4790abcd45557b301
Jone Polvora <jpolvora@gmail.com>
2023-02-27 02:17:15 -0400

    improving

--- CleanArch.Api/Models/AuthResponse.cs
+++ CleanArch.Api/Models/AuthResponse.cs
@@ -2,10 +2,12 @@ namespace CleanArch.Api.Models
 {
     public class AuthResponse
     {
-        public AuthResponse(string token)
+        public AuthResponse(string error, string token)
         {
+            Error = error;
             Token = token;
         }
         public string Token { get; private set; }
+        public string Error { get; private set; }
     }
 }
